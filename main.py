6c42b3be862206a8ef1d69e6f9a19bdcae36ae48
Blawer <blawermoncada@gmail.com>
2026-08-26 16:15:56 -0500

    Empleado creado

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from src.api.empleados import router as empleados_router
 from src.api.personas import router as personas_router
 from src.database.database import Base, engine
+from src.entities import empleados as _empleados_model
 from src.entities import personas as _personas_model
 
 
@@ -30,3 +32,4 @@ def inicio():
 
 
 app.include_router(personas_router)
+app.include_router(empleados_router)
